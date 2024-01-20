75fa8b8a990d3332163f264660423db8fe6ec126
BuildTools <unconfigured@null.spigotmc.org>
2024-01-20 14:25:37 +0100

    Initial commit

---
+++ src/main/java/mc/skykingpx/serverrules/ServerRules.java
@@ -0,0 +1,17 @@
+package mc.skykingpx.serverrules;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ServerRules extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
